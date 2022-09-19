84f051addfa163f0f94593d8fdb004081c736e50
asd24684782 <asd24684782@gmail.com>
2022-09-19 23:52:49 +0800

    play

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template>
-  <div class="app" data-width="20rem">
-    <input v-model="loginForm.username" type="text" placeholder="Username" data-space-bottom="0.5rem" />
-    <input v-model="loginForm.password" type="password" placeholder="Password" data-space-bottom="0.5rem" />
-    <br>
-    <button type="primary" @click="handleLogin">login</button>
-    <br>
-    <button @click="removeCookie">Clear</button>
-
-  </div>
+  <h1> Faker what was that !!! </h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResults" :score="score" />
 </template>
 
 <script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
+  components:{ Block, Result },
+
   data(){
     return {
-      loginForm : {
-          username: '',
-          password: '',
-          token: ''
-      }
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
     }
-  }
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+
+  },
+
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+
+
+</style>
