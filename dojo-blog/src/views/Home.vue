d3c0f725d0f0c154c227d7b9e041526aef78497f
asd24684782 <asd24684782@gmail.com>
2022-09-21 13:50:06 +0800

    compA

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+    <div v-if="error"> {{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else> Loading ... </div>
+  </div>
+  
+</template>
+
+<script>
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: { PostList },
+  setup() {
+
+    const { posts, error, load } = getPosts()
+    load()
+
+
+    return { posts, error }
+  },
+
+}
+</script>
